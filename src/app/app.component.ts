453c8cf3cf0aab4843b10bf593bfd2bc59c0b0cc
Mananbh98 <mananbhanushali98@gmail.com>
2020-10-31 10:15:45 +0530

    Prime or not in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prime';
+  number="";
+   p() {
+
+    var n, i, flag = true;
+
+    // Getting the value form text
+    // field using DOM
+    n = this.number;
+    n = parseInt(n)
+    for(i = 2; i <= n - 1; i++)
+        if (n % i == 0) {
+            flag = false;
+            break;
+        }
+
+        // Check and display alert message
+    if (flag == true)
+        console.log(n + " is prime");
+
+
+    else
+        console.log(n + " is not prime");
+
+
+    }
 }
