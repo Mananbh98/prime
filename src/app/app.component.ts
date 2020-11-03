209484ce10ae1741faf820ac6d420c981e99731b
Mananbh98 <mananbhanushali98@gmail.com>
2020-11-04 00:34:50 +0530

    Displayed whether the no. is prime or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,45 @@
+import { isPromise } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prime';
-  number="";
-   p() {
+  number = '';
+  currentVal = 0;
+  submitted = false;
+  isCurrentValPrime = false;
+  isCurrentPrime = '';
 
-    var n, i, flag = true;
-
-    // Getting the value form text
-    // field using DOM
-    n = this.number;
-    n = parseInt(n)
-    for(i = 2; i <= n - 1; i++)
-        if (n % i == 0) {
-            flag = false;
-            break;
-        }
-
-        // Check and display alert message
-    if (flag == true)
-        console.log(n + " is prime");
-
-
-    else
-        console.log(n + " is not prime");
-
-
+  onSubmit() {
+    this.submitted = true;
+    this.currentVal = parseInt(this.number);
+    this.isCurrentValPrime = this.isPrime(this.currentVal);
+    if (this.isCurrentValPrime === true) {
+      this.isCurrentPrime = 'prime';
+    } else {
+      this.isCurrentPrime = 'not prime';
     }
+    console.log(this.currentVal + ' is  ' + this.isCurrentPrime);
+  }
+  isPrime(val: number) {
+    var n: number,
+      i: number,
+      flag: boolean = true;
+
+    n = val;
+    for (i = 2; i <= n - 1; i++) {
+      if (n % i == 0) {
+        flag = false;
+        break;
+      }
+    }
+
+    // Check and display alert message
+
+    return flag;
+  }
 }
